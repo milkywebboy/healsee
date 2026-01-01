9dc2c514fc2331f9769e01a84540e2dd0b9c8a29
milkywebboy <milkywebboy@gmail.com>
2026-01-01 15:16:02 +0800

    2nd commit.

---
+++ healsee Watch AppTests/healsee_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  healsee_Watch_AppTests.swift
+//  healsee Watch AppTests
+//
+//  Created by yuta on 2026/01/01.
+//
+
+import Testing
+@testable import healsee_Watch_App
+
+struct healsee_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ healsee Watch AppUITests/healsee_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  healsee_Watch_AppUITestsLaunchTests.swift
+//  healsee Watch AppUITests
+//
+//  Created by yuta on 2026/01/01.
+//
+
+import XCTest
+
+final class healsee_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
